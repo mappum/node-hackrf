daee4ba5711d41d224971060401c765d36a97293
Alba Mendez <me@alba.sh>
2020-07-27 23:24:39 +0200

    Correct breaking calls

--- src/bindings.cc
+++ src/bindings.cc
@@ -6,7 +6,7 @@
 using namespace v8;
 
 void OpenDevice(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  int index = info[0]->Uint32Value();
+  int index = Nan::To<int>(info[0]).FromJust();
 
   hackrf_device_list_t* list = hackrf_device_list();
   if (index >= list->devicecount) return Nan::ThrowError("Invalid device index");
@@ -29,12 +29,12 @@ void Devices(const FunctionCallbackInfo<Value>& info) {
     Local<Object> device = Nan::New<Object>();
     enum hackrf_usb_board_id usb_board_id = list->usb_board_ids[i];
     const char* name = hackrf_usb_board_id_name(usb_board_id);
-    device->Set(Nan::New("name").ToLocalChecked(), Nan::New<String>(name).ToLocalChecked());
-    device->Set(Nan::New("usbIndex").ToLocalChecked(), Nan::New(list->usb_device_index[i]));
-    device->Set(Nan::New("usbBoardId").ToLocalChecked(), Nan::New(usb_board_id));
-    device->Set(Nan::New("serialNumber").ToLocalChecked(), Nan::New<String>(list->serial_numbers[i]).ToLocalChecked());
+    Nan::Set(device, Nan::New("name").ToLocalChecked(), Nan::New<String>(name).ToLocalChecked());
+    Nan::Set(device, Nan::New("usbIndex").ToLocalChecked(), Nan::New(list->usb_device_index[i]));
+    Nan::Set(device, Nan::New("usbBoardId").ToLocalChecked(), Nan::New(usb_board_id));
+    Nan::Set(device, Nan::New("serialNumber").ToLocalChecked(), Nan::New<String>(list->serial_numbers[i]).ToLocalChecked());
 
-    devices->Set(i, device);
+    Nan::Set(devices, i, device);
   }
 
   Nan::SetMethod(devices, "_open", OpenDevice);
@@ -42,7 +42,7 @@ void Devices(const FunctionCallbackInfo<Value>& info) {
   info.GetReturnValue().Set(devices);
 }
 
-void InitAll(Handle<Object> exports) {
+void InitAll(Local<Object> exports) {
   hackrf_init();
   Device::Init();
 
